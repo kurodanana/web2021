bf1418b24a7605ee1ab1293e1fe17e8422a0636e
kurodanana <zm18512@josai.ac.jp>
2021-12-21 02:12:37 +0000

    1221

--- ex6-10.js
+++ ex6-10.js
@@ -1,4 +1,5 @@
 function imgchange1()
- {document.getElementById('logo').src= './image/image.gif';}
+ {document.getElementById('logo').src= './image/soft.jpg';}
  function imgchange2()
- {document.getElementById('logo').src= './image/image.gif';}
+ {document.getElementById('logo').src= './image/josai.jpg';}
+
